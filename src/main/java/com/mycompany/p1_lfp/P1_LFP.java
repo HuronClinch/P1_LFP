d4e0dc8f2665518f4660ee2a6fd240dac1e0900d
HuronClinch <onljsd@gmail.com>
2025-09-29 10:54:15 -0600

    Creacion de proyecto

---
+++ src/main/java/com/mycompany/p1_lfp/P1_LFP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p1_lfp;
+
+/**
+ *
+ * @author huron_clinch
+ */
+public class P1_LFP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
